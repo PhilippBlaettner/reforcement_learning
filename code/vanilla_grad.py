4f452b03f356a413a456bb70a5cc2a813321bb00
Philipp Blaettner <blaettner.philipp@gmail.com>
2021-10-15 15:30:20 +0200

    use new version

--- code/vanilla_grad.py
+++ code/vanilla_grad.py
@@ -10,6 +10,7 @@ import numpy as np
 
 class SimpleNet(nn.Module):
     def __init__(self, input_size, hidden_size, action_size):
+        super(SimpleNet, self).__init__()
         self.fc1 = nn.Linear(input_size, hidden_size)
         self.fc2 = nn.Linear(hidden_size, action_size)
         self.action_size = action_size
@@ -31,17 +32,19 @@ class SimpleNet(nn.Module):
 class Trainer:
     """ A Trainer class run a simple 
     """
-    def __init__(self, agent, learning_rate, epoch_size, batch_size, sample_size, nr_epochs, mem_cap):
+    def __init__(self, agent,optimizer, learning_rate, epoch_size, batch_size, sample_size, nr_epochs, mem_cap, discount_rate, max_length):
         self.agent = agent
+        self.optimizer = optimizer(self.agent.parameters(),learning_rate)
         self.env = env = gym.make('CartPole-v0')
         self.learning_rate = learning_rate
         self.batch_size = batch_size
         self.epoch_size = epoch_size
         self.sample_size = sample_size
         self.nr_epochs = nr_epochs
+        self.discount_rate = discount_rate
+        self.max_length = max_length
 
         memory = deque(maxlen=mem_cap)
-
 
     def _collect_episode(env, agent, max_length, render=False):
         observation = env.reset()
@@ -84,20 +87,38 @@ class Trainer:
         pass
 
     def train_epoch(self):
+        av_loss = 0
+        for i in range(self.epoch_size):
+            batch = self._sample_batch()
+            self.optimizer.zero_grad()
+            av_loss += self.train_batch(batch)
+
+        return av_loss/self.epoch_size/self.batch_size
+
+    def train_batch(self, batch):
+        ob_batch, actions_batch, discounted_returns = batch
+        action_prob = self.agent(ob_batch)
+        loss = torch.sum(discounted_returns*torch.log(action_prob))
+        loss.backward()
+        self.optimizer_step()
+
         
-
-        return 0
-
-    def train_batch(self):
-        pass
 
     def _sample_batch(self):
         episodes = zip(*random.sample(self.memory, self.batch_size))
 
-        observations, actons, rewards = list(episodes)
+        observations, actions, rewards = list(episodes)
+        ob_batch = torch.cat(observations)
+        actions_batch = torch.cat(actions)
+        discounted_returns = self._estimate_return(rewards, self.discount_rate)
+        rewards_batch = torch.cat(discounted_returns)
+        return ob_batch, actions_batch, discounted_returns
 
-    def _estimate_return(self, rewards):
-        return rewards
+    def _estimate_return(self, rewards, gamma):
+        t_steps = np.arange(rewards.size)
+        r = rewards * gamma**t_steps
+        r = r[::-1].cumsum()[::-1] / gamma**t_steps
+        return r
 
     
     def _collect_data(self):
@@ -108,11 +129,15 @@ class Trainer:
 
 
 if __name__ == '__main__':
-    env = gym.make('CartPole-v0')
-    observation = env.reset()
-
-    for _ in range(1000):
-        env.render()
-        env.step(env.action_space.sample())
+    net = SimpleNet(4,16,2)
+    adam = torch.optim.Adam
+    learning_rate = 0.01
+    epoch_size = 10
+    batch_size = 16
+    sample_size = 100
+    nr_epochs = 10
+    mem_cap = 2000
+    discount_rate = 0.99
+    max_length = 200
+    Trainer(net, adam, learning_rate, epoch_size, batch_size, sample_size, nr_epochs, mem_cap, discount_rate, max_length)
     
-    env.close()
